a82fc4047464bd5cf6a1cd5e878dc01db678aa0c
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-04 19:15:03 +0200

    Font modal implemented

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,16 +1,38 @@
-import React from "react";
-import AppBar from "../../components/AppBar/AppBar";
+import React, { Component } from "react";
+import AppBar from "../AppBar/AppBar";
+import FontModal from "../FontModal/FontModal";
 import Styles from "./Layout.css";
+import Backdrop from "../Backdrop/Backdrop";
 
-const Layout = props => {
-  const { children } = props;
+class Layout extends Component {
+  state = {
+    showFontModal: false
+  };
 
-  return (
-    <div className={Styles.Layout}>
-      <AppBar />
-      <main>{children}</main>
-    </div>
-  );
-};
+  toggleFontModal() {
+    this.setState(prevState => ({
+      showFontModal: !prevState.showFontModal
+    }));
+  }
+
+  render() {
+    const { children } = this.props;
+    const { showFontModal } = this.state;
+
+    return (
+      <div className={Styles.Layout}>
+        <AppBar toggleFontModal={() => this.toggleFontModal()} />
+
+        {showFontModal && (
+          <Backdrop toggleModal={() => this.toggleFontModal()}>
+            <FontModal />
+          </Backdrop>
+        )}
+
+        <main>{children}</main>
+      </div>
+    );
+  }
+}
 
 export default Layout;
