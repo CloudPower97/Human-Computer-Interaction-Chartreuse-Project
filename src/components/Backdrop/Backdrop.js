2217c596993fca7716a646899c65ca6a1a029e03
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-04 19:15:30 +0200

    Backdrop Component for modals implemented

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Styles from "./Backdrop.css";
+import PropTypes from "prop-types";
+
+const Backdrop = props => {
+  const { children, toggleModal } = props;
+
+  return (
+    <div onClick={() => toggleModal()} className={Styles.Backdrop}>
+      {children}
+    </div>
+  );
+};
+
+Backdrop.propTypes = {
+  children: PropTypes.node.isRequired,
+  toggleModal: PropTypes.func.isRequired
+};
+
+export default Backdrop;
