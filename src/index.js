93155f57b0847adb433780a4c64c8989cde2fbba
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-09 11:10:34 +0200

    Modified URL and basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import * as serviceWorker from "./serviceWorker";
 
 const app = (
-  <BrowserRouter basename="/hci_project_v2">
+  <BrowserRouter basename="/hci_project">
     <App />
   </BrowserRouter>
 );
