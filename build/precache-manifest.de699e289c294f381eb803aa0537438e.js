63e9ac417bf3f92bd295b71daf5346e4d0c2213f
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-09 11:12:08 +0200

    First commit

---
+++ build/precache-manifest.de699e289c294f381eb803aa0537438e.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "95447906db2f4b5c32d5229b6e840432",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-small.95447906.webp"
+  },
+  {
+    "revision": "ef5174b4e03745e9c282",
+    "url": "/hci_project/static/css/main.5d833f7c.chunk.css"
+  },
+  {
+    "revision": "36469b728518af766df9",
+    "url": "/hci_project/static/js/1.36469b72.chunk.js"
+  },
+  {
+    "revision": "1a3b04751ed4ef7da566",
+    "url": "/hci_project/static/js/runtime~main.1a3b0475.js"
+  },
+  {
+    "revision": "5581d02ebbf9749caf6e3ec27584e663",
+    "url": "/hci_project/static/media/certosa-di-san-martino-small.5581d02e.jpg"
+  },
+  {
+    "revision": "1728e1197f7f0040920e5cf63998c53d",
+    "url": "/hci_project/static/media/certosa-di-san-martino-small@2x.1728e119.jpg"
+  },
+  {
+    "revision": "edb0b27a3a681dedaa7940ace9859d7c",
+    "url": "/hci_project/static/media/certosa-di-san-martino-small.edb0b27a.webp"
+  },
+  {
+    "revision": "e30aa2c8561a49fceab61f934c8cbc79",
+    "url": "/hci_project/static/media/certosa-di-san-martino-small@2x.e30aa2c8.webp"
+  },
+  {
+    "revision": "818ee9f0890e6c4e2a6251e9b25a1b64",
+    "url": "/hci_project/static/media/certosa-di-san-martino-large.818ee9f0.jpg"
+  },
+  {
+    "revision": "45daac74dc54d0bb7f55bcbe712cb5a0",
+    "url": "/hci_project/static/media/certosa-di-san-martino-large@2x.45daac74.jpg"
+  },
+  {
+    "revision": "563580b992cfc87d907248d09ea078da",
+    "url": "/hci_project/static/media/certosa-di-san-martino-large.563580b9.webp"
+  },
+  {
+    "revision": "eee64c10e0cb2e5195518e0383373fe0",
+    "url": "/hci_project/static/media/certosa-di-san-martino-large@2x.eee64c10.webp"
+  },
+  {
+    "revision": "f7254c8f41ffa0eb524cb60778643d8d",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-small.f7254c8f.jpg"
+  },
+  {
+    "revision": "ed8ccbc88f956031b2a44f43a7425736",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-small@2x.ed8ccbc8.jpg"
+  },
+  {
+    "revision": "ef5174b4e03745e9c282",
+    "url": "/hci_project/static/js/main.ef5174b4.chunk.js"
+  },
+  {
+    "revision": "4797633aadcfb54af61f31d3e955d86d",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-small@2x.4797633a.webp"
+  },
+  {
+    "revision": "16a47057f6845f2826e7e8907c52acac",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-large.16a47057.jpg"
+  },
+  {
+    "revision": "c9cf1a4c5a2ed80336dcf47d7f6d14d9",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-large@2x.c9cf1a4c.jpg"
+  },
+  {
+    "revision": "1e78b9b7a7f3cf0a3e51ff0c0be41dea",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-large.1e78b9b7.webp"
+  },
+  {
+    "revision": "44dde5d2526f8ea69a228e4e7e0c4676",
+    "url": "/hci_project/static/media/certosa-di-san-giacomo-large@2x.44dde5d2.webp"
+  },
+  {
+    "revision": "959821a092c49e273cf1bf935b65d411",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-small.959821a0.jpg"
+  },
+  {
+    "revision": "f23e74f5a08fa11c1c5ce56aa6032cfe",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-small@2x.f23e74f5.jpg"
+  },
+  {
+    "revision": "0ea36cfa24803fcd6894cc9812c28220",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-small.0ea36cfa.webp"
+  },
+  {
+    "revision": "0403391efc163395233bd58ee77f0324",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-small@2x.0403391e.webp"
+  },
+  {
+    "revision": "0107be82abf877066a69f661fe8bab55",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-large.0107be82.jpg"
+  },
+  {
+    "revision": "abccf74a8e1ba9b2faf22978d08720e7",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-large@2x.abccf74a.jpg"
+  },
+  {
+    "revision": "e99c0a1c442c3267807e1732bba4db08",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-large.e99c0a1c.webp"
+  },
+  {
+    "revision": "5870846c16ed670426247da1b026b06c",
+    "url": "/hci_project/static/media/certosa-di-san-lorenzo-large@2x.5870846c.webp"
+  },
+  {
+    "revision": "edb21544f8081f6a5386f0f8dad6759a",
+    "url": "/hci_project/index.html"
+  }
+];
