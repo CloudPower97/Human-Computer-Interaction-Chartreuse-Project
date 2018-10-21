0be6d73ee457a71dc7534b5cb1bdf51e11c7bef5
Claudio Cortese <claudio.cortese@outlook.it>
2018-10-21 15:48:11 +0200

    Using the newly created ChartreusesAnnotationBox component

--- src/components/ChartreusesEditor/ChartreusesEditor.js
+++ src/components/ChartreusesEditor/ChartreusesEditor.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import Styles from "./ChartreusesEditor.css";
+
+import ChartreuseAnnotationBox from "./ChartreusesAnnotationBox/ChartreusesAnnotationBox";
 class ChartreusesEditor extends Component {
   state = {
     _dropZones: []
@@ -253,52 +255,6 @@ class ChartreusesEditor extends Component {
 
   render() {
     const { annotationIndex } = this.state;
-    let annotationBox = null;
-    let annotationDescription =
-      "Una certosa è un monastero di monaci certosini, di norma situato in zone solitarie. Il nome deriva dalla Grande Certosa (Grande Chartreuse), monastero principale dell'Ordine Certosino, che si trova sul massiccio della Chartreuse sulle Alpi francesi a nord della città di Grenoble in Val-d'Isère.";
-    let annotationTitle = "Le Certose";
-
-    switch (annotationIndex) {
-      case 0:
-        annotationTitle = "Chiostro Grande";
-        annotationDescription =
-          "Intorno al chiostro grande sono disposte le celle dei padri, rappresentava pertanto il cuore della vita di clausura.";
-        break;
-
-      case 1:
-        annotationTitle = "Chiesa";
-        annotationDescription =
-          "Il luogo sacro per eccellenza dove i padri si recavano più volte al giorno, nel rispetto della liturgia certosina che prevede la preghiera notturna o Mattutino, la celebrazione della Messa conventuale e i Vespri.";
-        break;
-
-      case 2:
-        annotationTitle = "Celle";
-        annotationDescription =
-          "Le celle sono spazi sufficientemente ampi dove i monaci trascorrevano molto tempo, non solo vi dormivano e mangiavano, ma potevano dedicarsi alla preghiera solitaria e al lavoro, nel rispetto del motto ‘ora et labora’ cui è improntata la regola certosina. Nei giorni non festivi i padri ricevevano i pasti attraverso i passavivande, piccole aperture collocate accanto alla porta.";
-        break;
-
-      case 3:
-        annotationTitle = "Refettorio";
-        annotationDescription =
-          "È il luogo destinato a consumare i pasti in comunità, la domenica o nei giorni di festa. I monaci mangiavano in silenzio, ascoltando la sacra scrittura o altri testi indicati dal Priore, che venivano letti da un apposito pulpito.";
-        break;
-
-      case 4:
-        annotationTitle = "Quarto del Priore";
-        annotationDescription =
-          "Il Priore, capo spirituale della comunità certosina, disponeva di un quarto, un appartamento, più spazioso rispetto alle celle degli altri monaci e di solito riccamente decorato e dotato di giardino privato.";
-        break;
-
-      default:
-        break;
-    }
-
-    annotationBox = (
-      <React.Fragment>
-        <h2>{annotationTitle}</h2>
-        <p>{annotationDescription}</p>
-      </React.Fragment>
-    );
 
     return (
       <div id="editor" className={Styles.ChartreusesEditor}>
@@ -322,7 +278,6 @@ class ChartreusesEditor extends Component {
             <i className="fas fa-camera fa-stack-1x fa-inverse" />
           </span>
         </div>
-
         <div
           className={Styles.ChartreusesEditorModel}
           data-dropzone="second-model"
@@ -344,9 +299,7 @@ class ChartreusesEditor extends Component {
           </span>
         </div>
 
-        <div className={Styles.ChartreusesEditorDescription}>
-          {annotationBox}
-        </div>
+        <ChartreuseAnnotationBox annotationIndex={annotationIndex} />
       </div>
     );
   }
